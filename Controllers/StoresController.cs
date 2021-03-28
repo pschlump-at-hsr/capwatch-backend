fcf114eabb5f1d508d98b241f6b0c31e7a3a763a
Christoph Scheiwiller <christoph.scheiwiller@ost.ch>
2021-03-28 16:01:59 +0200

    fix: review Visual Studio Setup #CAPW-10

---
+++ Controllers/StoresController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace CapWatchBackend.Controllers {
+  [ApiController]
+  [Route("[controller]")]
+  public class StoresController : ControllerBase {
+    private readonly ILogger<StoresController> _logger;
+
+    public StoresController(ILogger<StoresController> logger) {
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public IEnumerable<Store> GetStores() {
+      List<Store> shops = new List<Store>();
+
+      shops.Add(new Store { Name = "Migros St. Gallen", CurrentCapacity = 70, MaxCapacity = 180 });
+      shops.Add(new Store { Name = "Säntispark Bäder", CurrentCapacity = 125, MaxCapacity = 150 });
+      shops.Add(new Store { Name = "Interdiscount", CurrentCapacity = 7, MaxCapacity = 26 });
+
+      return shops;
+    }
+  }
+}
