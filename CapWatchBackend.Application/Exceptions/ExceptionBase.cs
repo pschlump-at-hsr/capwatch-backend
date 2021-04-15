822d706c7c32e0d14ae6425581645de18bd5c08a
Pascal Schneider <pascal.schneider@ost.ch>
2021-04-15 20:35:57 +0200

    feat: implemented exception handling action filter

---
+++ CapWatchBackend.Application/Exceptions/ExceptionBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Net;
+
+namespace CapWatchBackend.Application.Exceptions {
+  public class ExceptionBase : ApplicationException {
+    public int Status { get; set; } = (int)HttpStatusCode.InternalServerError;
+
+    public ExceptionBase(string message) : base(message) { }
+    public ExceptionBase(string message, Exception innerException) : base(message, innerException) { }
+  }
+}
---
+++ CapWatchBackend.WebApi/ActionFilter/ExceptionFilter.cs
@@ -0,0 +1,33 @@
+﻿using CapWatchBackend.Application.Exceptions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CapWatchBackend.WebApi.ActionFilter {
+  public class ExceptionFilter : IActionFilter, IOrderedFilter {
+    private readonly IWebHostEnvironment _hostEnvironment;
+
+    public ExceptionFilter(IWebHostEnvironment hostEnvironment) {
+      _hostEnvironment = hostEnvironment;
+    }
+
+    public int Order { get; } = int.MaxValue;
+
+    public void OnActionExecuting(ActionExecutingContext context) { }
+
+    public void OnActionExecuted(ActionExecutedContext context) {
+      if (context.Exception is ExceptionBase exception) {
+        dynamic result;
+        if (_hostEnvironment.EnvironmentName.Equals("Development")) {
+          result = new { exception.Message, exception.StackTrace };
+        } else {
+          result = new { exception.Message };
+        }
+        context.Result = new ObjectResult(result) {
+          StatusCode = exception.Status
+        };
+        context.ExceptionHandled = true;
+      }
+    }
+  }
+}
