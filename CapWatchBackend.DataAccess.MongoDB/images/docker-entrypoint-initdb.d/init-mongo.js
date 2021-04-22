e576eb90d5ec70276c9781f9a970e53121070bdc
Pascal Schlumpf <pascal.schlumpf@ost.ch>
2021-04-22 11:38:38 +0200

    feat: moved passwords from init-mongo to config
    #CAPW-130

--- CapWatchBackend.DataAccess.MongoDB/images/docker-entrypoint-initdb.d/init-mongo.js
+++ CapWatchBackend.DataAccess.MongoDB/images/docker-entrypoint-initdb.d/init-mongo.js
@@ -1,15 +1,16 @@
+const config = require('./config');
 conn = new Mongo();
 admin = conn.getDB('admin');
 admin.auth('root', 'netwitness');
 admin.createUser({
-    user: 'capwadmin',
-    pwd: 'capwadmin123',
+    user: config.MONGO_ADMINNAME,
+    pwd: config.MONGO_ADMINPASSWORD,
     roles: ['userAdminAnyDatabase']
 });
 db = admin.getSiblingDB('capwatchDB');
 admin.createUser({
-    user: 'capwusr',
-    pwd: 'capwusr123',
+    user: config.MONGO_USERNAME,
+    pwd: config.MONGO_PASSWORD,
     roles: [{ role: "readWrite", db: "capwatchDB" }]
 });
 db.createCollection('stores');
