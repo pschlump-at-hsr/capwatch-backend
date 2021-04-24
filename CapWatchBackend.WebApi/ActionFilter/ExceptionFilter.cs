0c157cfa56670f00261e1d64ea3055be5d7511a2
Pascal Schneider <pascal.schneider@ost.ch>
2021-04-24 15:13:38 +0200

    fix: removed result body from exception response

--- CapWatchBackend.WebApi/ActionFilter/ExceptionFilter.cs
+++ CapWatchBackend.WebApi/ActionFilter/ExceptionFilter.cs
@@ -1,16 +1,9 @@
 ﻿using CapWatchBackend.Application.Exceptions;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace CapWatchBackend.WebApi.ActionFilter {
   public class ExceptionFilter : IActionFilter, IOrderedFilter {
-    private readonly IWebHostEnvironment _hostEnvironment;
-
-    public ExceptionFilter(IWebHostEnvironment hostEnvironment) {
-      _hostEnvironment = hostEnvironment;
-    }
-
     public int Order { get; } = int.MaxValue;
 
     public void OnActionExecuting(ActionExecutingContext context) {
@@ -19,17 +12,13 @@ namespace CapWatchBackend.WebApi.ActionFilter {
 
     public void OnActionExecuted(ActionExecutedContext context) {
       if (context.Exception is BaseException exception) {
-        dynamic result;
-        if (_hostEnvironment.EnvironmentName.Equals("Development")) {
-          result = new { exception.Message, exception.StackTrace };
-        } else {
-          result = new { exception.Message };
-        }
-        context.Result = new ObjectResult(result) {
+        context.Result = new ObjectResult(null) {
           StatusCode = exception.Status
         };
         context.ExceptionHandled = true;
       }
+
+
     }
   }
 }
