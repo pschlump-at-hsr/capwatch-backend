660c7dbf618cf583c703aed220eb6c72cd2000c4
Pascal Schlumpf <pascal.schlumpf@ost.ch>
2021-04-26 19:09:17 +0200

    fix: added testdata and passwords back

--- CapWatchBackend.DataAccess.MongoDB/images/docker-entrypoint-initdb.d/init-mongo.js
+++ CapWatchBackend.DataAccess.MongoDB/images/docker-entrypoint-initdb.d/init-mongo.js
@@ -1,11 +1,8 @@
-﻿const conn = new Mongo();
+﻿var MONGODB_USERNAME = 'capwusr';
+var MONGODB_USERPASSWORD = 'capwusr123';
+const conn = new Mongo();
 const admin = conn.getDB('admin');
 admin.auth('root', 'netwitness');
-admin.createUser({
-    user: MONGODB_ADMINUSER,
-    pwd: MONGODB_ADMINPASSWORD,
-    roles: ['userAdminAnyDatabase']
-});
 db = admin.getSiblingDB('capwatchDB');
 admin.createUser({
     user: MONGODB_USERNAME,
@@ -13,6 +10,11 @@ admin.createUser({
     roles: [{ role: "readWrite", db: "capwatchDB" }]
 });
 db.createCollection('stores');
+db.stores.insertMany([
+    { _id: UUID("9c9cee44-c839-48f2-b54e-237d95fe5d7f"), "secret": UUID("00000000-0000-0000-0000-000000000001"),"name": "Ikea", "street": "Zürcherstrasse 460", "zipCode": "9015", "city": "St. Gallen", "currentCapacity": 100, "maxCapacity": 201, "storeType": { "id": "f58957ce-fb83-4f62-ac2c-6d1fe810d85c", "description": "Bank" } },
+    { _id: UUID("9c9cee44-c839-48f2-b54e-238d95fe5d7f"), "secret": UUID("00000000-0000-0000-0000-000000000002"),"name": "Zoo Zürich", "street": "Zürichbergstrasse 221", "zipCode": "8044", "city": "Zürich", "currentCapacity": 528, "maxCapacity": 1125, "storeType": { "id": "7b0523b7-4efd-4fdf-b11d-3f4d26cf7b19", "description": "Freizeit" } },
+    { _id: UUID("9c9cee44-c839-48f2-b54e-239d95fe5d7f"), "secret": UUID("00000000-0000-0000-0000-000000000003"),"name": "Polenmuseum - Schloss Rapperswil", "street": "Zürcherstrasse 460", "zipCode": "9015", "city": "St. Gallen", "currentCapacity": 187, "maxCapacity": 201, "storeType": { "id": "f58957ce-fb83-4f62-ac2c-6d1fe810d85c", "description": "Bank" } }
+])
 db.createCollection('storeTypes');
 db.storeTypes.insertMany([
     { _id: UUID("c73e9c5f-de5c-479a-b116-7ee1b93ab4f9"), name: "Detailhändler" },
